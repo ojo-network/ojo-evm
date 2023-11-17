1027d12d62d7b0aee0bccb4e8843cbcb5b1f38bc
rbajollari <rbajollari@gmail.com>
2023-11-17 09:45:15 -0500

    fix: Update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,11 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const axelarGatewayAddress = "";
-  const axelarGasReceiverAddress = "";
+  const axelarGatewayAddress = "0xe432150cce91c13a887f7D836923d5597adD8E31"; // Goerli Testnet Address
+  const axelarGasReceiverAddress = "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6"; // Goerli Testnet Address
+  const ojoChain = "";
+  const ojoAddress = "";
+
 
   const Ojo = await ethers.getContractFactory("Ojo");
-  const ojo = await Ojo.deploy(axelarGatewayAddress, axelarGasReceiverAddress);
+  const ojo = await Ojo.deploy(axelarGatewayAddress, axelarGasReceiverAddress, ojoChain, ojoAddress);
 
   await ojo.waitForDeployment();
 
