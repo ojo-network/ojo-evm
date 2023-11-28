81516ec613b7fd49f2b0eea666c627644bef5594
rbajollari <rbajollari@gmail.com>
2023-11-28 14:36:40 -0500

    build: Deploy mock ojo contract in deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,15 +5,21 @@ async function main() {
   const axelarGasReceiverAddress = "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6"; // Goerli Testnet Address
   const ojoChain = "";
   const ojoAddress = "";
-
+  const resolveWindow = 1800
 
   const Ojo = await ethers.getContractFactory("Ojo");
-  const ojo = await Ojo.deploy(axelarGatewayAddress, axelarGasReceiverAddress, ojoChain, ojoAddress);
-
+  const ojo = await Ojo.deploy(axelarGatewayAddress, axelarGasReceiverAddress, ojoChain, ojoAddress, resolveWindow);
   await ojo.waitForDeployment();
+  const ojoContractddress = await ojo.getAddress();
+
+  const MockOjo = await ethers.getContractFactory("MockOjoContract");
+  const mockOjo = await MockOjo.deploy(ojoContractddress);
+  await mockOjo.waitForDeployment();
+  const mockOjoContractddress = await mockOjo.getAddress();
 
   console.log(
-    `Ojo deployed to ${await ojo.getAddress()}`
+    `Ojo deployed to ${ojoContractddress}\n`,
+    `MockOjo deployed to ${mockOjoContractddress}`
   );
 }
 
