db27db5c8581aee846988372abd578bfedde9ab0
Adam Wozniak <29418299+adamewozniak@users.noreply.github.com>
2024-01-23 17:24:38 -0800

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,12 +9,13 @@ let priv_key: string = process.env.PRIVATE_KEY || "";
 
 function get_networks(){
     let networks: any = {};
-    for (var chain of testnet_chains) {
-      networks[chain.name]={
-        network_id: chain.chainId,
-        accounts: [priv_key],
-        url: chain.rpc
-      }
+    if(priv_key !== "") {
+      for (var chain of testnet_chains) {
+        networks[chain.name]={
+          network_id: chain.chainId,
+          accounts: [priv_key],
+          url: chain.rpc
+        }
     }
 
     networks["hardhat"]={
