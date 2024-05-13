ce26902df09e3c891421d486bdecfa3cbf8fb318
rbajollari <rbajollari@gmail.com>
2024-05-13 09:07:22 -0600

    implement latestRound and getRoundData

--- contracts/pricefeed/CloneFactory.sol
+++ contracts/pricefeed/CloneFactory.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.20;
 import "./PriceFeed.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 
-/// @title Factory for creating PriceFeed contract clones
-/// @notice This contract will create a PriceFeed clone and map its address to clone creator
-/// @dev Cloning is done with OpenZeppelin's Clones contract and clones are initialized by an intitializer function instead of a constructor upon creation
+/// @title Factory for creating PriceFeed contract clones.
+/// @notice This contract will create a PriceFeed clone and map its address to the clone creator.
+/// @dev Cloning is done with OpenZeppelin's Clones contract.
 contract CloneFactory {
     event PriceFeedCloneCreated(
         address _priceFeedCloneAddress
@@ -15,15 +15,15 @@ contract CloneFactory {
     mapping (address => address) public PriceFeedCloneAddresses;
     address public implementationAddress;
 
-    /// @param _implementationAddress Address of implementation contract to be cloned
+    /// @param _implementationAddress Address of implementation contract to be cloned.
     constructor(address _implementationAddress) {
         implementationAddress = _implementationAddress;
     }
 
-    /// @notice Create clone of PriceFeed contract and initialize it
-    /// @dev Clone method returns address of created clone
+    /// @notice Create clone of PriceFeed contract and initialize it.
+    /// @dev Clone method returns address of created clone.
     /// @param _priceFeedDecimals Amount of decimals a PriceFeed is denominiated in.
-    /// @param _priceFeedDescription Description of PriceFeed.
+    /// @param _priceFeedDescription Description of PriceFeed, should be set to asset symbol ticker.
     function createPriceFeed(uint8 _priceFeedDecimals, string calldata _priceFeedDescription) external {
         address priceFeedCloneAddress = Clones.clone(implementationAddress);
         PriceFeed(priceFeedCloneAddress).initialize(_priceFeedDecimals, _priceFeedDescription);
